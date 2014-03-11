4ab7c5f6ffeed249faebec7969611d4a35d1e7df
Raphael Adam <raphael.adam@webfilings.com>
2014-03-11 15:43:19 -0500

    Adding function for modifying a list

--- class_exam/exam_functions.py
+++ class_exam/exam_functions.py
@@ -22,6 +22,28 @@ def read_list(users):
 
 
 def rearrange_list(u_name, u_list):
+    """
+
+    @param u_name: Name of person to bring to front of list
+    @param u_list: List of people
+    @return: Rearranged list of people with u_name in front
+    """
     i = u_list.index(u_name)
     u_list.insert(0, u_list.pop(i))
+    return u_list
+
+
+def modify_list(u_list):
+    u_input = raw_input("Would you like to add or remove a user? ").lower()
+    if u_input == "add":
+        n_input = raw_input("Enter a name to add: ")
+        u_list.append(n_input)
+    elif u_input == "remove":
+        try:
+            n_input = raw_input("Enter a name to remove: ")
+            u_list.remove(n_input)
+        except ValueError:
+            return "Specified user did not exist!"
+    else:
+        return "Invalid command, please type 'add' or 'remove'"
     return u_list
