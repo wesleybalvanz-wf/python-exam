6ab4e8fdbea9c056c86f72e7f0dbc67ff69f368f
Raphael Adam <raphael.adam@webfilings.com>
2014-03-14 11:26:47 -0500

    Adding tests for ShoppingList.remove_from_list()

--- tests/test_shopping_list.py
+++ tests/test_shopping_list.py
@@ -18,3 +18,21 @@ class TestAddToList(unittest.TestCase):
     def test_add_to_list(self):
         result = self.sample_list.add_to_list("Mangos")
         self.assertEqual(result, "Added Mangos to the list.")
+
+
+class RemoveFromList(unittest.TestCase):
+    def setUp(self):
+        l = ['Apples', 'Bananas', 'Oranges -Purchased', 'Watermelons']
+        self.sample_list = ShoppingList(l)
+
+    def test_remove_item(self):
+        result = self.sample_list.remove_from_list("Apples")
+        self.assertEqual(result, "Apples has been removed from the list.")
+
+    def test_purchased_item(self):
+        result = self.sample_list.remove_from_list("Oranges")
+        self.assertEqual(result, "You already bought Oranges.")
+
+    def test_not_on_list(self):
+        result = self.sample_list.remove_from_list("Kiwi")
+        self.assertEqual(result, "Kiwi is not on the list.")
