efa04eaaee807f3bbe6e92aa999bab57f0b6aeb1
wesleybalvanz-wf <wesley.balvanz@webfilings.com>
2014-03-17 09:48:14 -0500

    fixing self.user and self.opponent to be self.user_stats and self.opponent_stats

--- class_exam/fight_outcome.py
+++ class_exam/fight_outcome.py
@@ -17,21 +17,28 @@ class UserStats(object):
 
 
 class Outcome(UserStats, object):
+
     def __init__(self, user, opponent):
         self.user_name, self.user_stats = UserStats(user).get_name_and_stats()
         self.opponent_name, self.opponent_stats = UserStats(
             opponent).get_name_and_stats()
 
     def fight_outcome(self):
-        if self.user_stats > self.opponent_stats:
-            return "%s stats are %d and your stats are %d, you win!"\
-                   % (self.opponent_name, self.opponent_stats, self.user_stats)
-        elif self.user == self.opponent:
-            return "%s stats are %d and your stats are %d, it's a tie." \
-                   % (self.opponent_name, self.opponent_stats, self.user_stats)
-        elif self.user < self.opponent:
-            return "%s stats are %d and your stats are %d, you lose." \
-                   % (self.opponent_name, self.opponent_stats, self.user_stats)
+            if self.user_stats > self.opponent_stats:
+                return "%s stats are %d and your stats are %d, you win!"\
+                       % (self.opponent_name,
+                          self.opponent_stats,
+                          self.user_stats)
+            elif self.user_stats == self.opponent_stats:
+                return "%s stats are %d and your stats are %d, it's a tie." \
+                       % (self.opponent_name,
+                          self.opponent_stats,
+                          self.user_stats)
+            elif self.user_stats < self.opponent_stats:
+                return "%s stats are %d and your stats are %d, you lose." \
+                       % (self.opponent_name,
+                          self.opponent_stats,
+                          self.user_stats)
 
 
 def fight(opponents, user):
